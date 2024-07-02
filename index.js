1bf86ac49ccedb9969477b7c358b64636706b558
Ghost <103312649+Souvik-Ghost@users.noreply.github.com>
2024-07-02 17:09:01 +0000

    feat: index js modified

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const port = 3000;
-app.use(cors());
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const port = 3000;
+// app.use(cors());
+// app.listen(port, () => {
+//   console.log("Server running on port: " + port);
+// });
+let express = require('express');
+let app = express();
+let port = 3000;
 app.listen(port, () => {
-  console.log("Server running on port: " + port);
+  console.log(`server is running on port: ${port}`)
 });
+
 
 //1
 let person = {
